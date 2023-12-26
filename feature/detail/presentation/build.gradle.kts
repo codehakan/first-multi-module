bf82860b668404cf617c78a019b64b7b10cbef2e
Hakan AKKAYA <code.hakan@gmail.com>
2023-12-27 02:45:32 +0300

    detail modules was created. deleted test files.

---
+++ feature/detail/presentation/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.hakanbey.feature.detail.presentation"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(projects.navigation)
+
+    implementation(projects.common.data)
+    implementation(projects.common.presentation)
+    implementation(projects.feature.home.domain)
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.extensions)
+    implementation(libs.lifecycle.common)
+    implementation(libs.lifecycle.runtime)
+
+    //  Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
