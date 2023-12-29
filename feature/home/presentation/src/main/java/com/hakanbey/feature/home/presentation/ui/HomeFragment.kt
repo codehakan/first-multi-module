5361e7b6ebcd111ab0e6f702f4c85016adf3296f
Hakan AKKAYA <code.hakan@gmail.com>
2023-12-29 19:53:24 +0300

    added progressbar for home page.

--- feature/home/presentation/src/main/java/com/hakanbey/feature/home/presentation/ui/HomeFragment.kt
+++ feature/home/presentation/src/main/java/com/hakanbey/feature/home/presentation/ui/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.hakanbey.feature.home.presentation.ui
 
+import androidx.core.view.isGone
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.hakanbey.common.presentation.base.BaseFragment
@@ -21,7 +22,8 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
         binding.recyclerView.adapter = adapter
 
         adapter.setItemClickListener {
-            val direction = HomeFragmentDirections.actionHomeFragmentToDetailFragment(it.id.toString())
+            val direction =
+                HomeFragmentDirections.actionHomeFragmentToDetailFragment(it.id.toString())
             findNavController().navigate(direction)
         }
     }
@@ -30,6 +32,7 @@ class HomeFragment : BaseFragment(R.layout.fragment_home) {
         viewModel.getAllCharacters()
         viewModel.characterList.observe(viewLifecycleOwner) {
             adapter.submitList(it)
+            binding.progressCircular.isGone = true
         }
     }
 
